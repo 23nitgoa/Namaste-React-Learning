3e3e348e6bd18e4b9d01987ab4276bf08bcf6686
23nitgoa <23cse1037@nitgoa.ac.in>
2026-01-12 18:27:15 +0530

    Chapter 01

---
+++ App.js
@@ -0,0 +1,7 @@
+// React element(OBJECT)->HTML(Browser understand)
+// Create nested React Elements
+const parent =React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[ React.createElement("h1",{},"h1 tag"),React.createElement("h2",{},"h2 tag")]));
+// create root using createRoot
+const root =ReactDOM.createRoot(document.getElementById("root"));
+// Manipulate the HTML DOM 
+root.render(parent);
