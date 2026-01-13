7d2f4f11be81f27bfccffdfdabc519826e1146b7
23nitgoa <23cse1037@nitgoa.ac.in>
2026-01-13 22:06:20 +0530

    done

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // React element(OBJECT)->HTML(Browser understand)
 // Create nested React Elements
-const parent =React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[ React.createElement("h1",{},"h1 tag"),React.createElement("h2",{},"h2 tag")]));
+const parent =React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[ React.createElement("h1",{key:"h1"},"h1 tag"),React.createElement("h2",{ key:"h2"},"h2 tag")]));
 // create root using createRoot
 const root =ReactDOM.createRoot(document.getElementById("root"));
 // Manipulate the HTML DOM 
